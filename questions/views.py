ed0f4a4064af11eed41b803573f844d3d5984e3e
changho <ckdghdus@naver.com>
2019-10-07 10:23:51 +0900

    create 완성

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Question
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == "POST":
+        content = request.POST.get('content')
+        Question.objects.create(content=content)
+
+        return redirect('questions:index')  # 인덱스로 다시 보내준다.
+    else:
+        return render(request, 'form.html')
