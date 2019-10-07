c0eb256b05c74c9ae86e210f79c14a7a933532a1
changho <ckdghdus@naver.com>
2019-10-07 17:49:18 +0900

    1:N 만들기

--- questions/models.py
+++ questions/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+# 모델을 바꾸면 migrations
+
 
 class Question(models.Model):
     content = models.TextField()
+
+
+class Answer(models.Model):
+    content = models.CharField(max_length=100)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
